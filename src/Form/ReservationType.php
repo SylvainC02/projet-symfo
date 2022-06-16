8f239f2e4e4c9ae2e917ba3c7f1fdcee6a9808e1
SylvainC02 <sylvaincocqueret02@gmail.com>
2022-06-17 00:28:11 +0200

    oui

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,12 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('starting_date')
-            ->add('ending_date');
+            ->add('starting_date', DateType::class, [
+                'years' => ['2022', '2023', '2024']
+            ])
+            ->add('ending_date', DateType::class, [
+                'years' => ['2022', '2023', '2024']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
